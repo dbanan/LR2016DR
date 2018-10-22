0ddaaacbf77417e7fdc8f9aca25f12e950d483a2
DarshiBanan <banan.darshi@gmail.com>
2018-10-22 02:18:45 -0700

    started heritability script

---
+++ scripts/heritability_LR.R
@@ -0,0 +1,15 @@
+#heritability_LR.R
+
+
+#script to estimate heritability for LRS and culm height (height as comparison)
+
+
+
+#infile 
+load("./data/clean_data/data_population_score.Rdata")
+load("./data/clean_data/data_population_plant.Rdata")
+
+#visual_score
+#combine
+
+#for now just use rep1 v rep2 estimate 
