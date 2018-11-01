763af3d80df7d757b21c035246ad5d100055e2fe
DarshiBanan <banan.darshi@gmail.com>
2018-10-31 22:28:01 -0700

    Re coded score on snp phylogeny. output results

--- scripts/phylogenetic_analysis_LR.R
+++ scripts/phylogenetic_analysis_LR.R
@@ -22,9 +22,14 @@ load("data_population_score.Rdata")
 #list of genotypes used in experiment 
 genotypes<-unique(visual_score1$genotype)
 
-#trim phenotypes to just those of interest 
+#trim phenotypes to just those of interest (score4, 0-3)
 pheno<-subset(visual_score1, visual_score1$trait=="score4")
 pheno<-ddply(pheno, c("genotype"), summarise, score4=mean(data))
+
+#condense score further to three classes (no rolling, some rolling, severe rolling)
+pheno1<-pheno
+pheno1$score4[pheno1$score4>=0.5 & pheno1$score4<=1.5]<-1
+pheno1$score4[pheno1$score4>=2]<-2
 
 
 
@@ -48,7 +53,7 @@ snp_012<-subset(snp_012, rownames(snp_012) %in% genotypes)
 
 snp_pheno<-snp_012
 snp_pheno$genotype<-rownames(snp_pheno)
-snp_pheno<-merge(snp_pheno, pheno, by=c("genotype"))
+snp_pheno<-merge(snp_pheno, pheno1, by=c("genotype"))
 
 rownames(snp_pheno)<-snp_pheno$genotype
 snp_pheno$genotype<-NULL
@@ -61,14 +66,55 @@ d_snp<-dist(snp_pheno[,1:1316])
 hc_snp<-hclust(d_snp)
 
 
-plot(hc_snp)
+plot(hc_snp, hang=-1)
 
 
 dend<-as.dendrogram(hc_snp)
+plot(dend)
+
+
+#two ways to show denodrogram 
+#one way
+lab.color=matrix("black",nrow(snp_pheno))
+lab.color[which(score4==0)]="red"
+lab.color[which(score4==1)]="orange"
+lab.color[which(score4==2)]="blue"
+
+plot(as.phylo(hc_snp), cex = 0.6, tip.color = lab.color, label.offset = .75, type="fan")
+
+png("./results/phylo_score.png", height=900, width=900)
+plot(as.phylo(hc_snp), cex = 0.6, tip.color = lab.color, label.offset = .75, type="fan")
+dev.off()
+
+
+#another way
+dend=as.dendrogram(hc_snp)
+labels_colors(dend)=lab.color[order.dendrogram(dend)]
+dend<-color_branches(dend, k=5)
+plot(dend)
+circlize_dendrogram(dend, labels_track_height = NA, dend_track_height = .4)
+
+png("./results/phylo_score.png", height=1200, width=1200)
+circlize_dendrogram(dend, labels_track_height = NA, dend_track_height = .4)
+dev.off()
 
 
 
 
+
+
+
+
+
+#save these for now 
+circlize_dendrogram(dend, labels_track_height = NA, dend_track_height = .4)
+
+plot(as.phylo(hc.map), type = "fan", cex = 0.6, tip.color = lab.color, label.offset = .75)
+
+dend<-as.dendrogram(hc_snp)
+
+dend<-rotate(dend, 1:203)
+dend<-color_branches(dend, k=5)
 
 labels_colors(dend) <-
   rainbow_hcl(3)[sort_levels_values(
@@ -77,7 +123,19 @@ labels_colors(dend) <-
 
 labels(dend)<-as.character(snp_pheno[,1])[order.dendrogram(dend)]
 
+plot(dend)
+plot(dend, horiz =  TRUE)
+legend(legend = score4, fill = rainbow_hcl(3))
 
+#end save these for now
+
+
+
+
+
+
+
+#archive attempts 
 #output dendrograms 
 png("~/Desktop/meeting_dendrogram.png")
 plot(dend)
@@ -89,6 +147,12 @@ dev.off()
 png(file="./results/LRS_dendro.png")
 plot(as.phylo(hc_snp), tip.color=snp_pheno[,c(1317)])
 dev.off()
+
+plot(as.phylo(hc_snp), tip.color=snp_pheno[,c(1317)], type="fan")
+
+
+
+
 
 
 
